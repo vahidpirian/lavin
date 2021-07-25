bbc29066fb114f149b804e8949d2e04ed3d8ab27
vahidpirian <pirianit@gmail.com>
2021-07-25 18:54:31 +0430

    create .gitignorefile

--- lavin/contactinfo/views.py
+++ lavin/contactinfo/views.py
@@ -19,24 +19,24 @@ class contact_us(ListView):
         return Contact_Us.objects.first()
 
 
-def WhatsAppData(phone, message, name):
-    import time
-    import webbrowser as web
-    import pyautogui as pg
-    phone = "+98" + phone
-    web.open("https://web.whatsapp.com/send?=" + phone + "&text=" + message + "&name" + name)
-    time.sleep(30)
-    pg.press("enter")
-
-    def SendData(request):
-        if request.method == "POST":
-            Name = request.POST['FullName']
-            ph = request.POST['Phone']
-            Message = request.POST['Text']
-            WhatsAppData(ph, Message, Name)
-            msg = "پیغام با موفقیت ارسال شد.."
-            return render(request, "contact-us.html", {
-                "msg": msg
-            })
-        else:
-            return HttpResponse("<h1>404 - Not Found :)</h1>")
+# def WhatsAppData(phone, message, name):
+#     import time
+#     import webbrowser as web
+#     import pyautogui as pg
+#     phone = "+98" + phone
+#     web.open("https://web.whatsapp.com/send?=" + phone + "&text=" + message + "&name" + name)
+#     time.sleep(30)
+#     pg.press("enter")
+#
+#     def SendData(request):
+#         if request.method == "POST":
+#             Name = request.POST['FullName']
+#             ph = request.POST['Phone']
+#             Message = request.POST['Text']
+#             WhatsAppData(ph, Message, Name)
+#             msg = "پیغام با موفقیت ارسال شد.."
+#             return render(request, "contact-us.html", {
+#                 "msg": msg
+#             })
+#         else:
+#             return HttpResponse("<h1>404 - Not Found :)</h1>")
