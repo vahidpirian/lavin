734ee0f2965e6227e8551d2c13bfeecce96cbee7
vahidpirian <pirianit@gmail.com>
2021-07-29 10:45:06 +0430

    set mysql database

--- lavin/lavin/settings.py
+++ lavin/lavin/settings.py
@@ -83,10 +83,19 @@ DEFAULT_FILE_STORAGE = 'aboutinfo.models.ASCIIFileSystemStorage'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/conf.d/my.cnf',
+        },
     }
 }
 
